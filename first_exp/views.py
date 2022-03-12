75735023fb8e5dd0ad3efdb1d69e8ac2efe171b5
itu-itis21-karaagac21 <karaagac21@itu.edu.tr>
2022-03-12 21:10:48 +0300

    nav_bar added

--- first_exp/views.py
+++ first_exp/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"first_exp_templates/index.html")
+    return render(request,"first_exp/index.html")
 def pages(request):
-    return render(request,"first_exp_templates/page_lists.html")
+    return render(request,"first_exp/page_lists.html")
 def another_detail(request,id):
-    return render(request,"first_exp_templates/another_detail.html",{"id":id})
+    return render(request,"first_exp/another_detail.html",{"id":id})
