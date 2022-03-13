73a52a5c10969887bd946e71cc3d67a01b97fae3
itu-itis21-karaagac21 <karaagac21@itu.edu.tr>
2022-03-13 14:01:04 +0300

    links are dynamized

--- first_exp/views.py
+++ first_exp/views.py
@@ -1,10 +1,42 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+database = {
+    "pages":[
+        {
+            "id":1,
+            "title":"Page 1",
+            "image":"1.png",
+            "is_active":True,
+            "is_home":True,
+            "description":"This is page 1"
+        },
+        {
+            "id":2,
+            "title":"Page 2",
+            "image":"2.png",
+            "is_active":True,
+            "is_home":False,
+            "description":"This is page 2"
+        },
+        {
+            "id":3,
+            "title":"Page 3",
+            "image":"3.png",
+            "is_active":True,
+            "is_home":True,
+            "description":"This is page 3"
+        }
+    ]
+}
+
 # Create your views here.
 def index(request):
-    return render(request,"first_exp/index.html")
+    context = {"pages":database["pages"]}
+    return render(request,"first_exp/index.html",context)
 def pages(request):
-    return render(request,"first_exp/page_lists.html")
+    context = {"pages":database["pages"]}
+    return render(request,"first_exp/page_lists.html",context)
 def pages_detail(request,id):
     return render(request,"first_exp/pages_detail.html",{"id":id})
