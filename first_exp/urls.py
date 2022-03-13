228671250c0c3d62ac155575a3e3c919c1d44886
itu-itis21-karaagac21 <karaagac21@itu.edu.tr>
2022-03-13 14:07:04 +0300

    links are dynamized

--- first_exp/urls.py
+++ first_exp/urls.py
@@ -4,7 +4,7 @@ from . import views
 #url =  htttp://127.0.0.1:8000
 urlpatterns = [
     path("",views.index,name="home"),
-    path("index",views.index,name="index"),
+    path("index",views.index),
     path("pages",views.pages,name="pages"),
     path("pages/<int:id>",views.pages_detail,name="pages_detail")
 ]
