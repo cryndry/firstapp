b8379b88f3c84804e3fbfb8334c3250ecb003fa5
itu-itis21-karaagac21 <karaagac21@itu.edu.tr>
2022-03-15 14:53:45 +0300

    models are migrated

--- first_exp/models.py
+++ first_exp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    title= models.CharField(max_length=200)
+    image= models.CharField(max_length=200)
+    description= models.TextField(max_length=200)
+    is_active= models.BooleanField(default=False)
+    is_home= models.BooleanField(default=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
--- first_exp/views.py
+++ first_exp/views.py
@@ -2,42 +2,44 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-database = {
-    "pages":[
-        {
-            "id":1,
-            "title":"Page 1",
-            "image":"1.png",
-            "is_active":True,
-            "is_home":True,
-            "description":"This is page 1"
-        },
-        {
-            "id":2,
-            "title":"Page 2",
-            "image":"2.png",
-            "is_active":True,
-            "is_home":False,
-            "description":"This is page 2"
-        },
-        {
-            "id":3,
-            "title":"Page 3",
-            "image":"3.png",
-            "is_active":True,
-            "is_home":True,
-            "description":"This is page 3"
-        }
-    ]
-}
-
+# database = {
+#     "pages":[
+#         {
+#             "id":1,
+#             "title":"Page 1",
+#             "image":"1.png",
+#             "is_active":True,
+#             "is_home":True,
+#             "description":"This is page 1"
+#         },
+#         {
+#             "id":2,
+#             "title":"Page 2",
+#             "image":"2.png",
+#             "is_active":True,
+#             "is_home":False,
+#             "description":"This is page 2"
+#         },
+#         {
+#             "id":3,
+#             "title":"Page 3",
+#             "image":"3.png",
+#             "is_active":True,
+#             "is_home":True,
+#             "description":"This is page 3"
+#         }
+#     ]
+# }
+from first_exp.models import Page 
 # Create your views here.
 def index(request):
-    context = {"pages":database["pages"]}
+    context = {"pages":Page.objects.filter(is_home=True,is_active=True)}
     return render(request,"first_exp/index.html",context)
 def pages(request):
-    context = {"pages":database["pages"]}
+    context = {"pages":Page.objects.filter(is_active=True)}
     return render(request,"first_exp/page_lists.html",context)
 def pages_detail(request,id):
-    selectedPage = [page for page in database["pages"] if (page["id"] == id)][0]
+    # selectedPage = [page for page in database["pages"] if (page["id"] == id)][0]
+    # selectedPage = [page for page in Page.objects.all() if (page.filter(id) == id)][0]
+    selectedPage =  Page.objects.get(id=id)
     return render(request,"first_exp/pages_detail.html",{"page":selectedPage})
